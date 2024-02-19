26ba4cff49465c446d5d25e802503d66e03e6ceb
vikranth <91008254+Subham-git-Repo@users.noreply.github.com>
2024-02-19 23:33:08 +0530

    Added .env variaotiin

--- server.js
+++ server.js
@@ -6,27 +6,33 @@ const cors = require('cors'); // Import the cors middleware
 
 const app = express();
 const port = 3001;
+require("dotenv").config();
 
 app.use(bodyParser.json());
-app.use(cors({
-  origin: "*",
-})); // Use the cors middleware
+app.use(
+  cors({
+    origin: "*",
+  })
+); // Use the cors middleware
 
-app.post('/generate-response', async (req, res) => {
+app.post("/generate-response", async (req, res) => {
   const userInput = req.body.input;
-
+  const { CHATGPT_TOKEN } = process.env;
   try {
     // Call OpenAI GPT-3 API (replace YOUR_API_KEY with your actual OpenAI API key)
     const response = await axios.post(
-      'https://api.openai.com/v1/chat/completions',
+      "https://api.openai.com/v1/chat/completions",
       {
-        model: 'gpt-3.5-turbo',
-        messages: [{ role: 'system', content: 'You are a helpful assistant.' }, { role: 'user', content: userInput }],
+        model: "gpt-3.5-turbo",
+        messages: [
+          { role: "system", content: "You are a helpful assistant." },
+          { role: "user", content: userInput },
+        ],
       },
       {
         headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer 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`,
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${CHATGPT_TOKEN}`,
         },
       }
     );
@@ -36,8 +42,8 @@ app.post('/generate-response', async (req, res) => {
     // Send the generated response to the UI
     res.json({ response: generatedResponse });
   } catch (error) {
-    console.error('Error generating response:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error generating response:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
